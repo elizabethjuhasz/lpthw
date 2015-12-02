6e64b3db1a2f2f02ee275b3e31f4e67c85e49014
Elizabeth M Juhasz <juhasz.e.m@gmail.com>
2015-12-02 13:45:09 -0600

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,19 @@
+print "I will now count my chickens:"
+print "Hens", 25+30 / 6
+# print 30/6=5 5+25=30
+print "Roosters" , 100 - 25 * 3 % 4
+# print 25*3=75, then 75 / 4= 18 remainder 3 100-3=97
+print "Now I will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# print 3+2=5, 5+1=6, 6-5=1, 1+4=5, 5-1=4 4%2=0, 1+6=7
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7 
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater?" , 5 > -2
+print "Is it greater or equal?", 5>= -2
+print "Is it less or equal?" , 5 <= -2
+# print floating point means decimal. So if you put 1.0 it will treat it as a decimal.  
+print 1.0/4
